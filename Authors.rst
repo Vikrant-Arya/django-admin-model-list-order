d9f6ceddf7cda882756321cd5c1d66b03ad7d9da
386218 <VikrantArya@vikrantarya.local>
2021-07-27 01:36:50 +0530

    Added Manifest.in file

---
+++ Authors.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Vikrant Arya <vikrantarya21@gmail.com>
+
+Contributors
+------------
+
+* Vikrant Arya <vikrantarya21@gmail.com>
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import admin_model_list_order
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = admin_model_list_order.__version__
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='django-admin-model-list-order',
+    version=version,
+    description="""Custom ordering for models in the admin app.""",
+    long_description=readme + '\n\n' + history,
+    author='Vikrant Arya',
+    author_email='vikrantarya21@gmail.com',
+    url='https://github.com/Vikrant-Arya/django-admin-model-list-order',
+    packages=[
+        'admin_model_list_order',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'django'
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='django-admin-model-list-order',
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
